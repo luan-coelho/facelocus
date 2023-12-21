d74ea51b322d672278b52660eda3de8ce9746ef8
Luan Coêlho <lumyth.br@gmail.com>
2023-12-20 22:06:53 -0300

    feat: Tela inicial da aplicação.

--- mobile/lib/screens/home_screen.dart
+++ mobile/lib/screens/home_screen.dart
@@ -6,6 +6,27 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const options = [
+      FeatureCard(
+          description: "Registros de ponto",
+          route: "/point-record",
+          color: Colors.white,
+          backgroundColor: Color(0xFF003C84),
+          imageName: "point-record-icon.svg"),
+      FeatureCard(
+          description: "Eventos",
+          route: "/event",
+          color: Colors.black,
+          backgroundColor: Color(0xFFFFFFFF),
+          imageName: "event-icon.svg"),
+      FeatureCard(
+          description: "Solicitações de ingresso",
+          route: "/ticket-request",
+          color: Colors.black,
+          backgroundColor: Color(0xFFFAB411),
+          imageName: "ticket-request-icon.svg")
+    ];
+
     return Scaffold(
       appBar: AppBar(
           title: const Text("Facelocus", style: TextStyle(color: Colors.white)),
@@ -16,23 +37,24 @@ class HomeScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            SizedBox(
-              child: ListView.separated(
-                padding: const EdgeInsets.only(left: 50, right: 50),
-                separatorBuilder: (BuildContext context, int index) {
-                  return const SizedBox(height: 20);
-                },
-                scrollDirection: Axis.vertical,
-                shrinkWrap: true,
-                itemCount: 3,
-                itemBuilder: (context, index) {
-                  return const FeatureCard(
-                    description: "Registros de ponto",
-                    route: "",
-                    color: Color(0xFF003C84),
-                  );
-                },
-              ),
+            ListView.separated(
+              padding: const EdgeInsets.only(left: 30, right: 30),
+              separatorBuilder: (BuildContext context, int index) {
+                return const SizedBox(height: 20);
+              },
+              scrollDirection: Axis.vertical,
+              shrinkWrap: true,
+              itemCount: options.length,
+              itemBuilder: (context, index) {
+                FeatureCard option = options[index];
+                return FeatureCard(
+                  description: option.description,
+                  route: option.route,
+                  color: option.color,
+                  backgroundColor: option.backgroundColor,
+                  imageName: option.imageName,
+                );
+              },
             ),
           ],
         ),
--- mobile/lib/widgets/feature_card.dart
+++ mobile/lib/widgets/feature_card.dart
@@ -1,27 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:go_router/go_router.dart';
 
 class FeatureCard extends StatelessWidget {
   final String description;
   final String route;
-  final Color? color;
+  final Color color;
+  final Color backgroundColor;
+  final String imageName;
 
   const FeatureCard(
-      {super.key, required this.description, required this.route, this.color});
+      {super.key,
+      required this.description,
+      required this.route,
+      required this.color,
+      required this.backgroundColor,
+      required this.imageName});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 110,
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(8),
-      ),
+    return GestureDetector(
+      onTap: () => {
+        context.go('/dashboard')
+      },
       child: Container(
-        padding: const EdgeInsets.all(16),
+        height: 160,
+        decoration: BoxDecoration(
+          color: backgroundColor,
+          borderRadius: BorderRadius.circular(8),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.3),
+              spreadRadius: 0,
+              blurRadius: 5,
+              offset: const Offset(0, 1.5), // Shadow position
+            ),
+          ],
+        ),
         child: Center(
-          child: Text(
-            description,
-            style: const TextStyle(color: Colors.white, fontSize: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.asset(
+                'images/$imageName',
+                width: 50,
+                colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+              ),
+              const SizedBox(height: 5),
+              Text(
+                description,
+                style: TextStyle(
+                    color: color, fontSize: 20, fontWeight: FontWeight.w500),
+              ),
+            ],
           ),
         ),
       ),
