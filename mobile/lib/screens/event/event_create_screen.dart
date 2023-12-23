d8e8808aee890a5fad49c4cea923ea6bed7d3a57
Luan Coêlho <luancoelho.dev@gmail.com>
2023-12-22 23:57:29 -0300

    feat: Implementando tela de cadastro de evento.

--- mobile/lib/screens/event/event_create_screen.dart
+++ mobile/lib/screens/event/event_create_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:mobile/models/event.dart';
 import 'package:mobile/services/event_service.dart';
 import 'package:mobile/widgets/event/event_card.dart';
 
@@ -13,6 +12,7 @@ class EventCreateScreen extends StatefulWidget {
 class _EventCreateScreenState extends State<EventCreateScreen> {
   List<EventCard> events = [];
   late EventService _eventService;
+  bool isSwitched = false;
 
   @override
   void initState() {
@@ -23,13 +23,84 @@ class _EventCreateScreenState extends State<EventCreateScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text("Eventos")),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [],
-          ),
+      appBar: AppBar(title: const Text("Cadastrar evento")),
+      body: Padding(
+        padding:
+            const EdgeInsets.only(top: 20, right: 30, left: 30, bottom: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text("Descrição",
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
+            const SizedBox(height: 10),
+            TextFormField(
+              decoration: InputDecoration(
+                contentPadding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.black, width: 2),
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black.withOpacity(0.5)),
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+              ),
+            ),
+            const SizedBox(height: 15),
+            TextButton.icon(
+                onPressed: () => getDeviceLocation(),
+                label: const Text(
+                  "Localizações",
+                  style: TextStyle(color: Colors.black),
+                ),
+                icon:
+                    const Icon(Icons.location_on_rounded, color: Colors.black),
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        const Color(0xFFFAB411)))),
+            const SizedBox(height: 15),
+            Row(
+              children: [
+                const Flexible(
+                  child: Text(
+                      "Permitir que outros participantes enviem solicitações para ingresso",
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
+                ),
+                Switch(
+                    value: isSwitched,
+                    onChanged: (value) {
+                      setState(() {
+                        isSwitched = value;
+                      });
+                    }),
+              ],
+            ),
+            const SizedBox(height: 15),
+            SizedBox(
+              width: double.infinity, // Largura 100%
+              height: 50,
+              child: TextButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        const Color(0xFF003C84)),
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ))),
+                onPressed: () => {},
+                child: const Text("Cadastrar",
+                    style:
+                        TextStyle(fontWeight: FontWeight.w600, fontSize: 20)),
+              ),
+            ),
+          ],
         ),
+      ),
     );
   }
+
+  getDeviceLocation() {}
 }
