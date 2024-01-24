f44dbad717e5185a5d84d0bbd60da30d826e588b
Luan Coêlho <lumyth.br@gmail.com>
2024-01-24 01:15:50 -0300

    feat: update.

--- mobile/lib/shared/widgets/app_layout.dart
+++ mobile/lib/shared/widgets/app_layout.dart
@@ -1,8 +1,10 @@
+import 'package:facelocus/router.dart';
 import 'package:facelocus/shared/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:go_router/go_router.dart';
 
-class AppLayout extends StatelessWidget {
+class AppLayout extends StatefulWidget {
   const AppLayout(
       {super.key,
       this.appBarTitle,
@@ -16,21 +18,51 @@ class AppLayout extends StatelessWidget {
   final Widget? floatingActionButton;
 
   @override
+  State<AppLayout> createState() => _AppLayoutState();
+}
+
+class _AppLayoutState extends State<AppLayout> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    switch (index) {
+      case 0:
+        {
+          context.go(AppRoutes.home);
+          break;
+        }
+      case 1:
+        {
+          context.go(AppRoutes.event);
+          break;
+        }
+      case 2:
+        {
+          context.go(AppRoutes.eventTicketsRequest);
+          break;
+        }
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: showAppBar == null
+        appBar: widget.showAppBar == null
             ? AppBar(
                 centerTitle: true,
-                backgroundColor: appBarTitle != null
+                backgroundColor: widget.appBarTitle != null
                     ? AppColorsConst.blue
                     : AppColorsConst.white,
-                title: appBarTitle != null
+                title: widget.appBarTitle != null
                     ? Text(
-                        appBarTitle!,
+                        widget.appBarTitle!,
                         textAlign: TextAlign.center,
                         style: TextStyle(
                             fontSize: 16,
-                            color: appBarTitle != null
+                            color: widget.appBarTitle != null
                                 ? Colors.white
                                 : Colors.black),
                       )
@@ -42,7 +74,7 @@ class AppLayout extends StatelessWidget {
                           icon: SvgPicture.asset(
                             'images/chevron-left-icon.svg',
                             colorFilter: ColorFilter.mode(
-                                appBarTitle != null
+                                widget.appBarTitle != null
                                     ? Colors.white
                                     : Colors.black,
                                 BlendMode.srcIn),
@@ -52,7 +84,35 @@ class AppLayout extends StatelessWidget {
                       )
                     : null)
             : null,
-        body: body,
-        floatingActionButton: floatingActionButton);
+        body: widget.body,
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: AppColorsConst.blue,
+          items: [
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                'images/home-icon.svg',
+              ),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                'images/event-icon.svg',
+              ),
+              label: 'Eventos',
+            ),
+            BottomNavigationBarItem(
+                icon: SvgPicture.asset(
+                  'images/ticket-request-icon.svg',
+                ),
+                label: 'Solicitações',
+                backgroundColor: Colors.white),
+          ],
+          currentIndex: _selectedIndex,
+          selectedIconTheme: const IconThemeData(color: Colors.green),
+          selectedItemColor: Colors.amber[800],
+          selectedLabelStyle: const TextStyle(color: Colors.white),
+          onTap: _onItemTapped,
+        ),
+        floatingActionButton: widget.floatingActionButton);
   }
 }
