47fbae67ace421f29a59429b3451c9c57b405baf
Luan Coêlho <luancoelho.dev@gmail.com>
2024-02-17 18:02:13 -0300

    feat/test: update.

--- mobile/lib/models/user_face_photo_model.dart
+++ mobile/lib/models/user_face_photo_model.dart
@@ -1,20 +1,23 @@
 class UserFacePhotoModel {
   int id;
   String fileName;
-  DateTime uploadDate;
+  DateTime createdAt;
+  DateTime? updatedAt;
 
   UserFacePhotoModel({
     required this.id,
     required this.fileName,
-    required this.uploadDate,
+    required this.createdAt,
+    this.updatedAt,
   });
 
   factory UserFacePhotoModel.fromJson(Map<String, dynamic> json) {
     return UserFacePhotoModel(
       id: json['id'],
       fileName: json['fileName'],
-      uploadDate: DateTime.parse(json['uploadDate']),
-      // filePath: json['filePath'],
+      createdAt: DateTime.parse(json['createdAt']),
+      updatedAt:
+          json['updatedAt'] != null ? DateTime.parse(json['updatedAt']) : null,
     );
   }
 }
