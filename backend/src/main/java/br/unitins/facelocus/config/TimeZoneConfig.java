005538251e37c0b5d7b638ce1104f40b4dc3e06f
Luan Coêlho <luancoelho.dev@gmail.com>
2024-02-21 02:44:19 -0300

    feat/test: update.

---
+++ backend/src/main/java/br/unitins/facelocus/config/TimeZoneConfig.java
@@ -0,0 +1,13 @@
+package br.unitins.facelocus.config;
+
+import io.quarkus.runtime.StartupEvent;
+import jakarta.enterprise.event.Observes;
+import jakarta.inject.Singleton;
+
+//@Singleton
+public class TimeZoneConfig {
+
+    /*public void setTimezone(@Observes StartupEvent startupEvent) {
+        System.setProperty("user.timezone", "America/Sao_Paulo");
+    }*/
+}
