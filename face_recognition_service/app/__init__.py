ddabd0d4cb9d3122bd9d3a0b69bc356176290abd
Luan Coêlho <luancoelho.dev@gmail.com>
2024-02-26 01:21:40 -0300

    feat: Implementação de insightface.

--- face_recognition_service/app/__init__.py
+++ face_recognition_service/app/__init__.py
@@ -1,17 +1,17 @@
 from flask import Flask
 
-from app.df.views import df_bp
-from app.dl.views import dl_bp
-from app.fr.views import fr_bp
+from app.deepface.views import deepface_bp
+from app.insightface.views import insightface_bp
+from app.facerecognition.views import facerecognition_bp
 from error_handlers import register_error_handlers
 
 app = Flask(__name__)
 
 register_error_handlers(app)
 
-app.register_blueprint(fr_bp)
-app.register_blueprint(df_bp)
-app.register_blueprint(dl_bp)
+app.register_blueprint(facerecognition_bp)
+app.register_blueprint(deepface_bp)
+app.register_blueprint(insightface_bp)
 
 if __name__ == '__main__':
     app.run(debug=False)
---
+++ face_recognition_service/app/deepface/views.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, request, jsonify
+from deepface import DeepFace
+
+deepface_bp = Blueprint('deepface', __name__, url_prefix='/deepface')
+
+
+# https://github.com/serengil/deepface
+@deepface_bp.route('/check-faces', methods=['GET'])
+def df_check_faces():
+    # Verifica se o caminho das duas imagens foram enviadas
+    photo_face_path = request.args.get('photoFacePath')
+    profile_photo_face_path = request.args.get('profilePhotoFacePath')
+
+    # Fazer a comparação facial
+    result = DeepFace.verify(photo_face_path, profile_photo_face_path)
+    face_detected = bool(result['verified'])
+
+    # Verificar se as imagens são da mesma pessoa
+    return jsonify({'faceDetected': face_detected}), 200
