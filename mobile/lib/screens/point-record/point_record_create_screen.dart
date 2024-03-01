57db8a5ae4eca5b7d64adfa7f5d5f3f4e8af5a6b
Luan Coêlho <luancoelho.dev@gmail.com>
2024-03-01 02:54:19 -0300

    feat: update.

--- mobile/lib/screens/point-record/point_record_create_screen.dart
+++ mobile/lib/screens/point-record/point_record_create_screen.dart
@@ -11,10 +11,9 @@ import 'package:facelocus/shared/widgets/app_button.dart';
 import 'package:facelocus/shared/widgets/app_date_picker.dart';
 import 'package:facelocus/shared/widgets/app_delete_button.dart';
 import 'package:facelocus/shared/widgets/app_layout.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 
 class PointRecordCreateScreen extends StatefulWidget {
   const PointRecordCreateScreen({super.key});
@@ -30,7 +29,6 @@ class _PointRecordCreateScreenState extends State<PointRecordCreateScreen> {
   bool indoorLocationFactor = false;
   double _allowableRadiusInMeters = 5.0;
   late final RestorableDateTime _date;
-  late PointModel _point;
   List<PointModel> points = [];
   HashSet<Factor> factors = HashSet();
   EventModel? _event;
@@ -68,15 +66,6 @@ class _PointRecordCreateScreenState extends State<PointRecordCreateScreen> {
 
   @override
   Widget build(BuildContext context) {
-    newPoint() {
-      DateTime initialDate = _controller.initialDate.value ?? DateTime.now();
-      DateTime finalDate = _controller.finalDate.value ?? DateTime.now();
-      PointModel pointSaved = PointModel(
-          initialDate: initialDate.copyWith(), finalDate: finalDate.copyWith());
-      _point = PointModel(initialDate: initialDate, finalDate: finalDate);
-      _controller.points.add(pointSaved);
-    }
-
     return AppLayout(
         appBarTitle: 'Novo registro de ponto',
         body: SingleChildScrollView(
@@ -217,6 +206,7 @@ class MultiTimePicker extends StatefulWidget {
 }
 
 class _MultiTimePickerState extends State<MultiTimePicker> {
+  late final PointRecordController _controller;
   DateTime dateTime = DateTime.now();
   late TimeOfDay _startTime;
   late TimeOfDay _endTime;
@@ -240,12 +230,21 @@ class _MultiTimePickerState extends State<MultiTimePicker> {
   }
 
   void _addInterval() {
+    final DateTime now = DateTime.now();
+    DateTime initialDate = DateTime(
+        now.year, now.month, now.day, _startTime.hour, _startTime.minute);
+    DateTime finalDate =
+        DateTime(now.year, now.month, now.day, _endTime.hour, _endTime.minute);
+    PointModel point =
+        PointModel(initialDate: initialDate, finalDate: finalDate);
+    _controller.points.add(point);
     setState(() {
       timeIntervals.add({'initialDate': _startTime, 'finalDate': _endTime});
     });
   }
 
   void _removeInterval(int index) {
+    _controller.points.removeAt(index);
     setState(() {
       timeIntervals.removeAt(index);
     });
@@ -253,10 +252,11 @@ class _MultiTimePickerState extends State<MultiTimePicker> {
 
   @override
   void initState() {
-    super.initState();
+    _controller = Get.find<PointRecordController>();
     _startTime = TimeOfDay(hour: dateTime.hour, minute: dateTime.minute);
     DateTime endTime = dateTime.add(const Duration(minutes: 15));
     _endTime = TimeOfDay(hour: endTime.hour, minute: endTime.minute);
+    super.initState();
   }
 
   @override
@@ -265,7 +265,7 @@ class _MultiTimePickerState extends State<MultiTimePicker> {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         GestureDetector(
-          onTap: () => _selectTime(context, false),
+          onTap: () => _selectTime(context, true),
           child: Container(
               padding: const EdgeInsets.only(left: 15, right: 15),
               width: MediaQuery.of(context).size.width,
@@ -341,40 +341,48 @@ class _MultiTimePickerState extends State<MultiTimePicker> {
         const SizedBox(height: 10),
         const Text('Intervalos', style: TextStyle(fontWeight: FontWeight.bold)),
         const SizedBox(height: 5),
-        ListView.separated(
-          shrinkWrap: true,
-          itemCount: timeIntervals.length,
-          separatorBuilder: (BuildContext context, int index) {
-            return const SizedBox(height: 5);
-          },
-          itemBuilder: (context, index) {
-            final interval = timeIntervals[index];
-            return Container(
-                padding: const EdgeInsets.only(left: 15, right: 15),
-                width: double.infinity,
-                height: 45,
-                decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                    color: Colors.white),
-                child: Center(
-                    child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        const Icon(Icons.timelapse, color: Colors.black),
-                        const SizedBox(width: 5),
-                        Text(
-                            '${interval['initialDate']!.format(context)} - ${interval['finalDate']!.format(context)}',
-                            style:
-                                const TextStyle(fontWeight: FontWeight.w300)),
-                      ],
-                    ),
-                    AppDeleteButton(onPressed: () => _removeInterval(index))
-                  ],
-                )));
-          },
-        ),
+        Obx(() {
+          return ListView.separated(
+            shrinkWrap: true,
+            itemCount: _controller.points.length,
+            separatorBuilder: (BuildContext context, int index) {
+              return const SizedBox(height: 5);
+            },
+            itemBuilder: (context, index) {
+              final PointModel point = _controller.points[index];
+              return Container(
+                  padding: const EdgeInsets.only(left: 15, right: 15),
+                  width: double.infinity,
+                  height: 45,
+                  decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                      color: Colors.white),
+                  child: Center(
+                      child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          const Icon(Icons.timelapse, color: Colors.black),
+                          const SizedBox(width: 5),
+                          Builder(builder: (context) {
+                            final DateFormat formatter = DateFormat('HH:mm');
+                            final String initialDate =
+                                formatter.format(point.initialDate);
+                            final String finalDate =
+                                formatter.format(point.finalDate);
+                            return Text('$initialDate - $finalDate',
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.w300));
+                          }),
+                        ],
+                      ),
+                      AppDeleteButton(onPressed: () => _removeInterval(index))
+                    ],
+                  )));
+            },
+          );
+        }),
       ],
     );
   }
